a2045af4fe86b0c976a775f7a6d00d678ec34fb1
Vladislav Danilov <vadanilov@dev3.ru>
2025-03-14 16:50:21 +0400

    Update script

--- python/test_script.py
+++ python/test_script.py
@@ -17,14 +17,17 @@ def read_data():
       if connected < -1:
           print("Not Connected")
           return 
-      distance = lidar.getDistance()
-      velocity = lidar.getVelocity()
-      print(f"Дистанция: {distance};    Скорость: {velocity}")
-          
-      elapsed_time = time.time() - start_time
-      timestamps.append(elapsed_time)
-      distances.append(distance)
-      velocities.append(velocity)
+      if not lidar.exit_requested("q"):
+        distance = lidar.getDistance()
+        velocity = lidar.getVelocity()
+        print(f"Дистанция: {distance};    Скорость: {velocity}")
+            
+        elapsed_time = time.time() - start_time
+        timestamps.append(elapsed_time)
+        distances.append(distance)
+        velocities.append(velocity)
+      else:
+        lidar.generate_graph(timestamps, distances, velocities)
 
   except Exception as e:
     print(f"[ERROR] {e}")
